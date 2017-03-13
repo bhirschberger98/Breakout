f1ed35947cd74941d86d1b148498e702e918a987
bhirschberger98 <bretthirschberger@gmail.com>
2017-03-13 10:43:26 -0400

    Added UI elements

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
 
     public Vector2 startingVelocity = new Vector2(15, -20);
     private Vector3 startingPosition;
-    public GameObject gameOversign;
+    public GameObject Game_Over;
     public GameObject youwinsign;
+    UnityEngine.UI.Text livesValue;
+    public Text pointsValue;
+
     int lives = 3;
+    int points = 0;
 
     void Start()
     {
         startingPosition = transform.position;
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        livesValue.text = lives.ToString();
     }
 
 
@@ -34,6 +40,7 @@ public class Ball : MonoBehaviour
     {
         Debug.Log("You are out");
         lives = lives - 1;
+        livesValue.text = lives.ToString();
 
         transform.position = startingPosition;
         GetComponent<Rigidbody2D>().velocity = new Vector2();
@@ -47,11 +54,13 @@ public class Ball : MonoBehaviour
 
     void DoGameOver()
     {
-        gameOversign.SetActive(true);
+        Game_Over.SetActive(true);
     }
     public void Brick_break()
     {
         var bricks_left = FindObjectsOfType<Brick>().Length;
+        points += 1;
+        pointsValue.text = points.ToString();
         if (bricks_left == 0)
         {
             youwinsign.SetActive(true);
